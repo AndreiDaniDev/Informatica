f8b1f499db6161102debd5e7c52db5c431159c6f
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-06-17 20:26:30 +0300

    Create 1223_paint.cpp

---
+++ 1223_paint.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+///https://www.pbinfo.ro/probleme/1233/paint
+using namespace std;
+ifstream in("paint.in");
+ofstream out("paint.out");
+int n, k, m, v[250002], qa, qb, rez = 0;
+int main(){
+    in>>n>>k>>m;
+    for(int i = 0; i < m; i++)
+        in>>qa>>qb, v[qa]++, v[qa + qb]--;
+    for(int i = 1; i < n; i++)
+        v[i] += v[i - 1], rez += (v[i] < k);
+    rez += (v[0] < k);
+    out<<rez;
+    return 0;
+}

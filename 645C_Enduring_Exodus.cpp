d42d1ce2f796a307c22346db70f4a78376291c94
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-06-19 13:32:04 +0300

    Update 645C_Enduring_Exodus.cpp

--- 645C_Enduring_Exodus.cpp
+++ 645C_Enduring_Exodus.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <vector>
-///https://codeforces.com/contest/645/submission/266431384
+///https://codeforces.com/contest/645/submission/266432644
 using namespace std;
-int n, k, m, dist, st, dr; vector <int> v; char s;
+int n, k, m, dist = 200004; vector <int> v; char s;
 int main(){
-    cin >> n >> k; dist = 2 * n;
-    for(int p = 0; p < n && cin>>s; p++) 
+    cin >> n >> k;
+    for(int p = 0; p < n && cin>>s; p++)
         if(s == '0') v.push_back(p);
-    
+ 
     for(int p = k; p < v.size(); p++) {
         for(; v[m] - v[p - k] <= v[p] - v[m]; m++);
-        st(v[m] - v[p - k]), dr(v[p] - v[m]), mx = (st > dr) ? st : dr;
-        dist = (dist < mx) ? dist : mx; st = v[m - 1] - v[p - k]; dr = v[p] - v[m - 1];
-        mx = (st > dr) ? st : dr; dist = (dist < mx) ? dist : mx;
+        dist = min(dist, max(v[m] - v[p - k], v[p] - v[m]));
+        dist = min(dist, max(v[m - 1] - v[p - k], v[p] - v[m - 1]));
     }
-    
+ 
     cout << dist;
     return 0;
 }

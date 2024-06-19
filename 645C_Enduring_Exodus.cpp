8a3916f51356e781ee6727be0584bdac7d942a86
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-06-19 13:23:41 +0300

    Update 645C_Enduring_Exodus.cpp

--- 645C_Enduring_Exodus.cpp
+++ 645C_Enduring_Exodus.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 ///https://codeforces.com/contest/645/submission/266431384
 using namespace std;
-int n, k, m, dist; vector <int> v; char s;
+int n, k, m, dist, st, dr; vector <int> v; char s;
 int main(){
     cin >> n >> k; dist = 2 * n;
     for(int p = 0; p < n && cin>>s; p++) 
@@ -10,7 +10,7 @@ int main(){
     
     for(int p = k; p < v.size(); p++) {
         for(; v[m] - v[p - k] <= v[p] - v[m]; m++);
-        int st(v[m] - v[p - k]), dr(v[p] - v[m]), mx = (st > dr) ? st : dr;
+        st(v[m] - v[p - k]), dr(v[p] - v[m]), mx = (st > dr) ? st : dr;
         dist = (dist < mx) ? dist : mx; st = v[m - 1] - v[p - k]; dr = v[p] - v[m - 1];
         mx = (st > dr) ? st : dr; dist = (dist < mx) ? dist : mx;
     }

73ba8bcbe0e01358ac3aef2d4638d4b592ae1040
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-07-15 18:22:33 +0300

    Update 744_rec.cpp

--- 744_rec.cpp
+++ 744_rec.cpp
@@ -7,11 +7,11 @@ ofstream out("rec.out");
 ///dp[i] = nr modalitati de a impartii premile
 int64_t s, n, f, dp[402];
 int main(){
-	in>>s>>n>>f; s -= n * f;
-	for(int i = 0; i <= s; i++) dp[i] = 1;
-		for(int i = 2; i <= n; i += 1)
-			for(int j = i; j <= s; j += 1)
-				dp[j] += dp[j - i];
-	out<<dp[s];
-	return 0;
+    in>>s>>n>>f; s -= n * f;
+    for(int i = 0; i <= s; i++) dp[i] = 1;
+    for(int i = 2; i <= n; i += 1)
+        for(int j = i; j <= s; j += 1)
+            dp[j] += dp[j - i];
+    out<<dp[s];
+    return 0;
 }

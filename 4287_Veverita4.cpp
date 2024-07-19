279befe8ff5ac064fa66accf30524cb422cfb9d6
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-07-19 20:49:48 +0300

    Update 4287_Veverita4.cpp

--- 4287_Veverita4.cpp
+++ 4287_Veverita4.cpp
@@ -4,9 +4,30 @@ ifstream in("veverita4.in");
 ofstream out("veverita4.out");
 
 ///https://www.pbinfo.ro/probleme/4287/veverita4
-///https://www.pbinfo.ro/detalii-evaluare/51598977
+///https://www.pbinfo.ro/detalii-evaluare/51606573
 
-typedef int NrMare[1002];
+///FAST FIBONACCI 
+struct Mat{ int mat[2][2]; };
+const Mat nullMat={{{1, 0},{0, 1}}};
+const Mat initMat={{{1, 1},{1, 0}}};
+
+Mat prod(Mat a,Mat b){
+    Mat ret;
+    ret.mat[0][0] = (1LL * a.mat[0][0] * b.mat[0][0] + 1LL * a.mat[0][1] * b.mat[1][0] ) % 666013;
+    ret.mat[0][1] = (1LL * a.mat[0][0] * b.mat[0][1] + 1LL * a.mat[0][1] * b.mat[1][1] ) % 666013;
+    ret.mat[1][0] = (1LL * a.mat[1][0] * b.mat[0][0] + 1LL * a.mat[1][1] * b.mat[1][0] ) % 666013;
+    ret.mat[1][1] = (1LL * a.mat[1][0] * b.mat[0][1] + 1LL * a.mat[1][1] * b.mat[1][1] ) % 666013;
+    return ret;
+}
+
+Mat pwr(Mat mat,int n){
+    if(!n) return nullMat;
+    if(n & 1) return prod(mat,pwr(prod(mat,mat),n>>1));
+    return pwr(prod(mat,mat),n>>1);
+}
+
+///BIG NUMBERS
+typedef int NrMare[10002];
 void Suma(NrMare a, NrMare b, NrMare c){
     int i, t = 0; c[0] = max(a[0], b[0]);
     for(i = 1; i <= c[0]; i++, t /= 10)
@@ -27,32 +48,22 @@ void Afis(NrMare x){
         out<<x[i]; out<<"\n";
 }
 
-int n, k, t1, t2, t3; NrMare dp1 = {1, 4}, dp2 = {1, 6}, dp3;
+int n, k; NrMare dp1 = {1, 4}, dp2 = {1, 6}, dp3;
 int main(){
     in>>n;
     if(n <= 1000){
         if(n <= 2){
-            (n == 1) ? out<<4 : out<<6;
+            (n == 1) ? out<<4 : out<<6; 
         }else{
             for(int i = 3; i <= n; i++){
                 Suma(dp1, dp2, dp3);
                 Egalare(dp1, dp2);
                 Egalare(dp2, dp3);
             }
-            Afis(dp3);
+            Afis(dp3);   
         }
     }else{
-        t1 = 249651; t2 = 34433;
-
-        if(n == 1001){
-            out<<t1;
-        }else if(n == 1002){
-            out<<t2;
-        }else{
-            for(int i = 1003; i <= n; i++)
-                t3 = (t1 + t2) % 666013, t1 = t2, t2 = t3;
-            out<<t3;
-        }
+        out<<pwr(initMat,n + 2).mat[0][1] * 2 % 666013;
     }
     return 0;
 }

19b693f2e41627a094d82d165b67289edad213dc
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-07-21 09:13:40 +0300

    Create 1093_text.cpp

---
+++ 1093_text.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
+ifstream in("text.in");
+ofstream out("text.out");
+
+///https://www.pbinfo.ro/probleme/1093/text
+///https://www.pbinfo.ro/detalii-evaluare/51611441
+///https://kilonova.ro/submissions/405861
+
+string s[20002], it; vector <int> sol;
+int n, lmax, poss, lastPos['z' - 'a' + 2], dp[20002], previous[20002];
+
+int main(){
+	for(n = 1; in>>s[n]; n++); n--;
+
+	for(int i = 1; i <= n; i++){
+        it = s[i];
+        dp[i] = dp[lastPos[it[0] - 'a']] + 1;
+        previous[i] = lastPos[it[0] - 'a'];
+        if(dp[i] > dp[lastPos[it[it.size() - 1] - 'a']])
+            lastPos[it[it.size() - 1] - 'a'] = i;
+	}
+
+	for(int i = 1; i <= n; i++)
+        if(lmax < dp[i])
+            lmax = dp[i], poss = i;
+
+	///out<<n - lmax<<"\n"<<n;
+    out<<n<<"\n"<<n - lmax<<"\n";
+
+    sol.push_back(poss);
+    poss = previous[poss];
+    for(; poss; ){
+        sol.push_back(poss);
+        poss = previous[poss];
+    }
+
+    for(int i = sol.size() - 1; i >= 0; i--)
+        out<<s[sol[i]]<<"\n";
+	return 0;
+}

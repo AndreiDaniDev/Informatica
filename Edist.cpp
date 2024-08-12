43192050a4f719d830bb9982bbad29f1e358fff6
AndreiDani <89001893+AndreiIPIT@users.noreply.github.com>
2024-08-12 10:40:32 +0300

    Update Edist.cpp

--- Edist.cpp
+++ Edist.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 ifstream in("edist.in");
 ofstream out("edist.out");
-string s, t; int n, m, k;
 ///https://www.infoarena.ro/problema/edist
-int64_t dp[2][102];
+///https://www.infoarena.ro/job_detail/3240040
+string s, t; int n, m, k, mini, maxi;
+int64_t dp[2][20002];
 int main(){
     in>>n>>m>>k>>s>>t;
     for(int j = 1; j <= t.size(); j++) dp[0][j] = j;
 
     for(int i = 1; i <= s.size(); i++){
-        dp[i & 1][0] = i;
-        for(int j = 1; j <= t.size(); j++){
+        dp[i & 1][0] = i; mini = min((int)t.size(), i + k); maxi = max(1, i - k);
+        for(int j = maxi; j <= mini; j++){
             dp[i & 1][j] = min(1 + min(dp[(i - 1) & 1][j], dp[i & 1][j - 1]),
                                dp[(i - 1) & 1][j - 1] + (s[i - 1] != t[j - 1]));
         }
